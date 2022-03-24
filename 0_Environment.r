c625eeff09b51c6aa094c4493fb35fd817edf968
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2022-03-24 19:47:44 -0400

    main code relies less on dist.r and can deal with discontinuous stuff

--- 0_Environment.r
+++ 0_Environment.r
@@ -29,91 +29,77 @@ pathRelease = 'https://drive.google.com/drive/folders/1I6nMmo8k_zGCcp9tUvmMedKTA
 target_dribble = pathRelease %>% drive_ls() 
 
 
+# GLOBAL VARIABLS ====
+
+
 # FUNCTIONS ====
-# translate par0$true* into a distr object
-#   must be a less ugly way to do this but I dunno what it is
-#   to find a list of distributions, use ??distr or see page 7 of long manual (distribution classes)
-create_mutrue_object = function(par){
-  
-  if (par$truefam == 'norm'){
-    
-    str.o = 'Norm(par$truepar1, par$truepar2)'
-    
-  } else if (par$truefam == 'lognorm'){
-    
-    # truepar1 = E(mu) and truepar2 = sd(mu), use wikipedia formulas to translate
-    mu = log( par$truepar1^2/sqrt(par$truepar2^2+par$truepar1^2) )
-    sig = sqrt(log(1+par$truepar2^2/par$truepar1^2))
-    str.o = paste0('exp(Norm(mu,sig))')
-    
-  } else if (par$truefam == 'foldedt'){
-    
-    str.o = 'abs(par$truepar1 + par$truepar2*Td(par$truepar3))'
-    
-  } else if (par$truefam == 'exp'){
-    str.o = 'Exp(1/par$truepar1)'
-  }
-  
-  return = eval(parse(text = str.o))
-}
 
-# prob of publication
-prob_pub = function(tt,par){
+import_cz = function(dl = F){
   
-  if (par$pubfam == 'piecelin'){
+  if (dl) {
+    # download signal documentation 
+    target_dribble = pathRelease %>% drive_ls() %>% 
+      filter(name=='SignalDocumentation.xlsx')
     
-    prob = 0.5 + par$tslope*(tt-par$tmid)
-    prob[prob<0] = 0; prob[prob>1] = 1
-    return = prob    
+    drive_download(target_dribble, path = 'temp/deleteme.xlsx', overwrite = T)
     
-  } else if (par$pubfam == 'logistic'){
+    signaldoc = left_join(
+      read_excel('temp/deleteme.xlsx',sheet = 'BasicInfo')  
+      , read_excel('temp/deleteme.xlsx',sheet = 'AddInfo')
+    ) %>% 
+      mutate(
+        signalname = Acronym
+        , pubdate = as.Date(paste0(Year, '-12-31'))
+        , sampend = as.Date(paste0(SampleEndYear, '-12-31'))
+        , sampstart = as.Date(paste0(SampleStartYear, '-01-01'))
+      ) %>% 
+      arrange(signalname) %>% 
+      select(signalname, pubdate, sampend, sampstart)
     
-    # this family may be required if mutrue is student's t
-    prob = 1/(1+exp(-par$tslope*(tt-par$tmid)))
+    # download returns and add samptype
+    target_dribble = pathRelease %>% drive_ls() %>%
+      filter(name=='Portfolios') %>% drive_ls() %>%
+      filter(name=='Full Sets OP') %>% drive_ls() %>%
+      filter(name=='PredictorLSretWide.csv')
     
-  } else if (par$pubfam == 'stair'){
-    # does not work
-    prob = (tt > 1.96 & tt <= 2.58)*par$tslope +(tt>2.58)*1
-    # prob = (tt > 1.96)*1
+    drive_download(target_dribble[1,], path = 'temp/deleteme2.csv', overwrite = T)
     
-  } else if (par$pubfam == 'trunc'){
-    prob = 1*(tt > par$tmid)
+    ret = fread('temp/deleteme2.csv') %>% 
+      pivot_longer(
+        -date, names_to = 'signalname', values_to = 'ret'
+      ) %>% 
+      filter(!is.na(ret)) %>% 
+      left_join(signaldoc, by = 'signalname') %>% 
+      mutate(
+        samptype = case_when(
+          (date >= sampstart) & (date <= sampend) ~ 'in-samp'
+          , (date > sampend) & (date <= pubdate) ~ 'out-of-samp'
+          , (date > pubdate) ~ 'post-pub'
+          , TRUE ~ NA_character_
+        )
+      ) %>% 
+      select(-pubdate,-sampend,-sampstart)
     
-  } # end if par$pubfam
+    # summarize into pubcross
+    pubcross = ret %>% 
+      filter(samptype == 'in-samp') %>% 
+      group_by(signalname) %>% 
+      summarize(
+        rbar = mean(ret), vol = sd(ret), ndate = dplyr::n()
+      ) %>% 
+      mutate(
+        tstat = rbar/vol*sqrt(ndate), tabs = abs(tstat)
+      )
+    
+    save(pubcross, file = 'temp/pubcross.Rdata')
+    
+  } # if dl 
+    
+  load(file = 'temp/pubcross.Rdata') # loads pubcross
   
+  return = pubcross
   
-} # end prob_pub
-
-
-# function for creating object of |t| cond on publication
-par2observed = function(par){
-  # create tabspub.o = |t| cond on publication in steps
-  #   1) make all t
-  t.o =UnivarMixingDistribution(
-    Norm(0,1), create_mutrue_object(par) + Norm(0,1)
-    , mixCoeff = c(par$pif, 1-par$pif)
-  ) 
-  #   2) take abs
-  tabs.o = abs(t.o)
-  
-  #   3) condition on publication
-  # doc here: https://www.rdocumentation.org/packages/distr/versions/2.8.0/topics/AbscontDistribution
-  
-  if (par$pubfam != 'trunc'){
-    tabspub.o = AbscontDistribution(
-      d=function(tt) d(tabs.o)(tt)*prob_pub(tt,par)
-      , withStand = 1
-      , low = par$tmin
-      , low1 = par$tmin
-    )
-    
-  } else {
-    tabspub.o = Truncate(tabs.o, lower = par$tmid)
-  } # if par$pubfam 
-  
-  return = tabspub.o
-  
-} # par2observed
+} # end download_cz
 
 # function: translate par df to parvec vector form
 par2parvec = function(par, namevec){
@@ -127,187 +113,38 @@ parvec2par = function(parvec,parbase,namevec){
   return(par)
 } # end parvec2par
 
-# lower and upper boudns of search space
-parveclim = function(namevec){
-  par.lb = data.frame(
-    pif = 0.001, truepar1 = 0, truepar2 = 0, truepar3 = 0, tmid = 0, tslope = 0
+# selection (assumed)
+prob_pub = function(tt, par){
+  (tt > 1.96 & tt <= 2.60)*par$tslope + (tt > 2.6)   
+}
+
+# latent t-stats
+make_t.o = function(par){
+  t.o = UnivarMixingDistribution(
+    Norm(0,1), Norm(par$mua,sqrt(par$siga^2+1)), Norm(par$mub,sqrt(par$sigb^2+1))
+    , mixCoeff = c(par$pif, (1-par$pif)*par$pia, (1-par$pif)*(1-par$pia))
   )
-  par.ub = data.frame(
-    pif = 1, truepar1 = 100, truepar2 = 100, truepar3 = 100, tmid = 3, tslope = 100
-  )
-  
-  out = list(
-    lb = par.lb %>% select(all_of(namevec)) %>% as.numeric()
-    , ub = par.ub %>% select(all_of(namevec)) %>% as.numeric()
-  )
-  
-  return = out
-} # end parveclim
-
-# function for turning two series into a histogram plot.  
-# This should be default in r but why is this so hard
-plot_2hist = function(x1,x2,edge = seq(0,15,0.5), x1name = 'x1', x2name = 'x2'){
-  datall = tibble(
-    group = x1name, x = x1
-  ) %>% rbind(
-    tibble(
-      group = x2name, x = x2
-    )
-  )
-  
-  hall = datall %>% 
-    filter(x>min(edge), x<max(edge)) %>% 
-    group_by(group) %>% 
-    summarise(
-      tmid = hist(x,edge)$mid
-      , density = hist(x,edge)$density
-    ) 
-  
-  hall %>% 
-    ggplot(aes(x=tmid, y=density)) +
-    geom_bar(
-      aes(fill = group), stat = 'identity', position = 'dodge'
-    )
-  
-} # end plot 2 hist
+  tabs.o = abs(t.o)
+  return = tabs.o
+}
 
 
-
-
-one_estimate = function(est,tabspub,opts=opts.base){
+# likelihood
+negloglike = function(parvec){
+  par = parvec2par(parvec,par.guess,namevec)
   
-  # -1* the log likelihood
-  negloglike = function(parvec){
-    par = parvec2par(parvec,est$parguess,est$namevec)
-    
-    # create observed t-stat object
-    tabspub.o = par2observed(par)
-    
-    onelike = d(tabspub.o)(tabspub)
-    onelike[onelike <= 0 ] = 1e-6 # might be a better way to do this
-    out = -1*mean(log( onelike ))
-    return = out
-    
-  } # end negloglike
+  # latent
+  tabs.o = make_t.o(par)
   
-  parvecguess = par2parvec(est$parguess, est$namevec)
+  # observable
+  #   take on data
+  tt = samp$tabs
   
-  # optimize
-  opt = nloptr(
-    x0 = parvecguess
-    , lb = parveclim(est$namevec)$lb
-    , ub = parveclim(est$namevec)$ub
-    , eval_f = negloglike
-    , opts = opts
-  )
+  #   find single likelihoods
+  denom = par$tslope*(p(tabs.o)(2.6)-p(tabs.o)(1.96)) + (1-p(tabs.o)(2.6))
+  onelikes = d(tabs.o)(tt)*prob_pub(tt,par)/denom
   
-  # pack output nicely along with settings
-  est$loglike = -opt$objective
-  est$par    = parvec2par(opt$solution, est$parguess, est$namevec) 
+  onelikes[onelikes<=0] = 1e-6
+  return = -1*mean(log(onelikes))
   
-  return = est
-  
-} # end one_estimate
-
-# estimate pi_f on a grid
-pifgrid_estimate = function(est,tabspub,opts = opts.base){
-  
-  # estimate at all pifs on a grid, coarsely
-  for (i in 1:length(est$pifgrid)){
-    
-    # modify one_estimate settings
-    tempest = est
-    tempest$namevec = tempest$namevec[tempest$namevec != 'pif'] # remove pif from namevec
-    tempest$parguess$pif = est$pifgrid[i] # replace pif guess with current grid
-    
-    tempest = one_estimate(tempest,tabspub,opts)
-    temppar = tempest$par %>% 
-      mutate(loglike = tempest$loglike)
-    
-    if ( i == 1 ){
-      estgrid = temppar
-    } else {
-      estgrid = rbind(estgrid,temppar)
-    } # if i == 1
-    
-    # feedback
-    # temppar %>% print()
-    
-  } # for i
-  
-  # find the pif that has the best loglike and estimate again, but finely
-  ibest = which.max(estgrid$loglike)
-  tempest = est
-  tempest$parguess = estgrid[ibest, ] %>% select(-loglike)
-  estfinal = one_estimate(tempest,tabspub,opts)
-  
-  # pack up and output
-  out = list(
-    grid = estgrid
-    , est = estfinal
-  )
-  return = out
-  
-} # end grid_estimate
-
-
-# calculate t-hurdle from model
-find_hurdle = function(par){
-  hlist = seq(0,5,0.01)
-  
-  t.o =UnivarMixingDistribution(
-    Norm(0,1), create_mutrue_object(par) + Norm(0,1)
-    , mixCoeff = c(par$pif, 1-par$pif)
-  ) 
-  
-  fdrdat = tibble(
-    h = hlist
-    , prob_h_f = 2*pnorm(-hlist)
-    , prob_h = 1-p(t.o)(hlist)
-    , fdr_h = prob_h_f/prob_h*par$pif
-  )    
-  
-  hstar = fdrdat %>% filter(fdr_h <= 0.05) %>% summarize(hstar = min(h)) %>% pull(hstar)
-  
-  return = hstar
-  
-} # end find_hurdle
-
-plot_emp_vs_mod = function(mod,emptabs){
-  
-  edge = c(seq(0,15,1), 40)
-  freq = numeric(length(edge)-1)
-  for (i in 2:length(edge)){
-    a = edge[i-1]; b = edge[i]
-    freq[i-1] = p(mod)(b) - p(mod)(a)
-  }
-  
-  hemp = hist(emptabs, edge, plot = F)
-  
-  plotme = tibble(
-    group = 'emp', tabs = hemp$mids, freq = hemp$density
-  ) %>% rbind(
-    tibble(
-      group = 'mod', tabs = hemp$mids, freq = freq    
-    )
-  )
-  
-  stat = plotme %>% 
-    pivot_wider(names_from = group, values_from = freq) %>% 
-    mutate(
-      error = 1000*(mod - emp)
-    ) 
-  
-  print(stat)
-  print(mean(abs(stat$error)))
-  
-  plotme %>% 
-    ggplot(aes(x=tabs,y=freq,group=group,color=group)) +
-    geom_line() +
-    geom_point(size=3) +
-    theme_minimal() +
-    scale_color_manual(values = c('blue','red')) + 
-    theme(legend.position = c(8,8)/10) +
-    xlim(0,15)  
-  
-} # end plot_emp_vs_mod
+} # end f_obs
--- 1_Main.r
+++ 1_Main.r
@@ -12,253 +12,115 @@ min_per_boot
 
 # DOWNLOAD DATA, MAKE PUBCROSS ====
 
-# download signal documentation 
-target_dribble = pathRelease %>% drive_ls() %>% 
-  filter(name=='SignalDocumentation.xlsx')
+pubcross = import_cz(dl = F)
 
-drive_download(target_dribble, path = 'temp/deleteme.xlsx', overwrite = T)
-
-signaldoc = left_join(
-  read_excel('temp/deleteme.xlsx',sheet = 'BasicInfo')  
-  , read_excel('temp/deleteme.xlsx',sheet = 'AddInfo')
-) %>% 
-  mutate(
-    signalname = Acronym
-    , pubdate = as.Date(paste0(Year, '-12-31'))
-    , sampend = as.Date(paste0(SampleEndYear, '-12-31'))
-    , sampstart = as.Date(paste0(SampleStartYear, '-01-01'))
-  ) %>% 
-  arrange(signalname) %>% 
-  select(signalname, pubdate, sampend, sampstart)
-
-# download returns and add samptype
-target_dribble = pathRelease %>% drive_ls() %>%
-  filter(name=='Portfolios') %>% drive_ls() %>%
-  filter(name=='Full Sets OP') %>% drive_ls() %>%
-  filter(name=='PredictorLSretWide.csv')
-
-drive_download(target_dribble[1,], path = 'temp/deleteme2.csv', overwrite = T)
-
-ret = fread('temp/deleteme2.csv') %>% 
-  pivot_longer(
-    -date, names_to = 'signalname', values_to = 'ret'
-  ) %>% 
-  filter(!is.na(ret)) %>% 
-  left_join(signaldoc, by = 'signalname') %>% 
-  mutate(
-    samptype = case_when(
-      (date >= sampstart) & (date <= sampend) ~ 'in-samp'
-      , (date > sampend) & (date <= pubdate) ~ 'out-of-samp'
-      , (date > pubdate) ~ 'post-pub'
-      , TRUE ~ NA_character_
-    )
-  ) %>% 
-  select(-pubdate,-sampend,-sampstart)
-
-# summarize into pubcross
-pubcross = ret %>% 
-  filter(samptype == 'in-samp') %>% 
-  group_by(signalname) %>% 
-  summarize(
-    rbar = mean(ret), vol = sd(ret), ndate = dplyr::n()
-  ) %>% 
-  mutate(
-    tstat = rbar/vol*sqrt(ndate), tabs = abs(tstat)
-  )
+# ESTIMATE ====
 
 # filter 
-#   remove 24 tabs < 1.96.  Modeling these doesn't pass cost / benefit
-pubcross = pubcross %>% filter(tabs > 1.96)
-
-
-
-# SET BASELINE BY HAND ====
-
-source('0_Environment.r')
-# lognorm: (2, 2, NA) fits well
-# foldedt: (0, 0.5/0.2, 5) fits well
-# exp (2, NA, NA)
+#   remove 24 tabs < 1.96.  Modeling these may not pass cost / benefit
+samp = pubcross %>% filter(tabs > 1.96)
 
 # model parameters
-par.hand = data.frame(
-  pif = 0.4
-  , truefam  = 'exp' # 'norm' or 'lognorm' or 'foldedt'
-  , truepar1 = 2
-  , truepar2 = NA
-  , truepar3 = NA
-  , pubfam  = 'logistic' # 'piecelin' or 'logistic' or 'stair' or 'trunc'
-  , tmid    = 2.25
-  , tslope  = 10
-  , tmin    = 1
+par.guess = data.frame(
+  pif = 0.1
+  , pia = 0.25
+  , mua = 2
+  , siga = 0.1
+  , mub = 5
+  , sigb = 2
+  , tslope = 0.5
+)
+par.lb = data.frame(
+  pif = 0.001
+  , pia = 0.001
+  , mua = 0.5
+  , siga = 0.001
+  , mub = 1
+  , sigb = 0.0001
+  , tslope = 0.3
+)
+par.ub = data.frame(
+  pif = 0.99
+  , pia = 0.99
+  , mua = 4
+  , siga = 10
+  , mub = 10
+  , sigb = 10
+  , tslope = 1
 )
 
-# make model tabs | pub object
-tabspub.g.o = par2observed(par.hand)
 
-# plot
-p1 = plot_emp_vs_mod(tabspub.g.o, pubcross$tabs)
-tt = seq(1,3,0.001)
-p3 = tibble(
-  tt = tt, prob = prob_pub(tt,par.hand)
+# parameters to estimate
+namevec = c('pif', 'pia','mua','mub','siga','sigb')
+
+# test objective
+negloglike(par2parvec(par.guess,namevec))
+
+# opts
+opts = list(
+  algorithm = 'NLOPT_GN_CRS2_LM' # 'NLOPT_LN_BOBYQA' or 'NLOPT_GN_DIRECT_L' or 'NLOPT_GN_CRS2_LM'
+  , xtol_rel = 0.001
+  , print_level = 3
+  , maxeval = 100
+)
+
+opt = nloptr(
+  x0 = par2parvec(par.guess, namevec)
+  , lb = par2parvec(par.lb, namevec)
+  , ub = par2parvec(par.ub, namevec)
+  , eval_f = negloglike
+  , opts = opts
+)
+parvec.hat = opt$solution
+par.hat = parvec2par(parvec.hat,par.guess,namevec)
+
+## plot result ====
+par = par.hat
+
+# latent 
+tabs.o = make_t.o(par)
+
+# observable
+denom = par$tslope*(p(tabs.o)(2.6)-p(tabs.o)(1.96)) + (1-p(tabs.o)(2.6))
+f_obs = function(tt) d(tabs.o)(tt)*prob_pub(tt,par)/denom
+
+# setup
+edge = c(seq(0,15,1), 40)
+freq = numeric(length(edge)-1)
+for (i in 2:length(edge)){
+  a = edge[i-1]; b = edge[i]
+  freq[i-1] = integrate(f_obs, a, b)$value
+}
+
+hemp = hist(pubcross$tabs, edge, plot = F)
+
+plotme = tibble(
+  group = 'emp', tabs = hemp$mids, freq = hemp$density
+) %>% rbind(
+  tibble(
+    group = 'mod', tabs = hemp$mids, freq = freq    
+  )
 ) %>% 
-  ggplot(aes(x=tt,y=prob)) + geom_line() +theme_minimal()
+  filter(!is.na(freq))
 
-grid.arrange(p1,p3, nrow = 1)
+stat = plotme %>% 
+  pivot_wider(names_from = group, values_from = freq) %>% 
+  mutate(
+    error = 1000*(mod - emp)
+  ) 
 
+print(stat)
+print(mean(abs(stat$error)))
 
-# ONE ESTIMATE ====
-source('0_Environment.r')
-# doesn't work that great, unsurprising since because of the theorem
-# pif often stays close to the initial guess
+plotme %>% 
+  ggplot(aes(x=tabs,y=freq,group=group,color=group)) +
+  geom_line() +
+  geom_point(size=3) +
+  theme_minimal() +
+  scale_color_manual(values = c('blue','red')) + 
+  theme(legend.position = c(0.8,0.8)) +
+  xlim(0,15)  
 
-# estimation settings
-est.point = list(
-  parguess = par.hand %>% mutate(pif = 0.3)
-  , namevec = c('pif', 'truepar1') # names of parameters to estimate  
-)
+par
 
-# estimate
-tempopts = opts.base
-tempopts$xtol_rel = 0.001
-tempopts$ftol_rel = 0.01
-est.point = one_estimate(est.point, pubcross$tabs, tempopts)
-
-# plot
-tabspub.o = par2observed(est.point$par)
-p1 = plot_2hist(
-  pubcross$tabs, r(tabspub.o)(1e3), seq(0,18,1), 'data', 'model'
-)
-
-mu = create_mutrue_object(est.point$par)
-p2 = tibble(mu = r(mu)(1e3)) %>% 
-  ggplot(
-    aes(x=mu)
-  ) + 
-  geom_histogram(aes(y=stat(density)))
-
-tt = seq(0,4,0.25)
-p3 = tibble(
-  tt = tt, prob = prob_pub(tt,est.point$par)
-) %>% 
-  ggplot(aes(x=tt,y=prob)) + geom_point()
-
-grid.arrange(p1,p2,p3, nrow = 1)
-
-# output to console
-est.point
-
-# GRID ESTIMATE ====
-
-source('0_Environment.r')
-
-tic = Sys.time()
-
-# estimation settings
-est.grid = list(
-  parguess = par.hand
-  , namevec = c('pif', 'truepar1')
-  , pifgrid = c(0.001, seq(0.05,0.95,0.05))
-)
-
-tempopts = opts.base
-tempopts$print_level = 0
-tempopts$xtol_rel = 0.0001
-tempopts$ftol_rel = 0.001
-
-# estimate
-est.grid =  pifgrid_estimate(est.grid, pubcross$tabs, tempopts)
-
-# plot 
-tabspub.o = par2observed(est.grid$est$par)
-p1 = plot_2hist(
-  pubcross$tabs, r(tabspub.o)(1e3), seq(0,18,1), 'data', 'model'
-)
-
-mu = create_mutrue_object(est.grid$est$par)
-p2 = tibble(mu = r(mu)(1e3)) %>%
-  ggplot(
-    aes(x=mu)
-  ) +
-  geom_histogram(aes(y=stat(density)))
-
-p3 = est.grid$grid %>% ggplot(aes(x=pif,y=loglike)) + geom_point()
-
-tt = seq(0,4,0.25)
-p4 = tibble(
-  tt = tt, prob = prob_pub(tt,est.grid$est$par)
-) %>% 
-  ggplot(aes(x=tt,y=prob)) + geom_point()
-
-grid.arrange(p1,p2,p3,p4, nrow = 2)
-
-# output to console
-est.grid
-
-toc = Sys.time()
-
-toc - tic
-
-# test plots ====
-
-# plot point estimate
-tempmod = par2observed(est.grid$est$par)
-plot_emp_vs_mod(tempmod, pubcross$tabs)
-
-# plot pif = 0.30 estimate
-temppar = est.grid$grid %>% filter(pif == 0.3)
-plot_emp_vs_mod(par2observed(temppar), pubcross$tabs)
-
-
-# BOOTSTRAP ====
-nboot = 100
-nsignal = nrow(pubcross)
-set.seed(1217)
-
-id_all = matrix(
-  sample(1:nsignal, size = nsignal*nboot, replace = T), nrow = nboot 
-)
-
-# estimation settings: use point estimate as initial guess
-est.boot = list(
-  parguess = par.hand
-  , namevec = c('pif', 'truepar1')  
-  , pifgrid = seq(0.05,0.95,0.05)
-)
-
-tempopts = opts.base 
-tempopts$print_level = 0
-tempopts$xtol_rel = 0.0001
-tempopts$ftol_rel = 0.001
-
-for (booti in 1:nboot){
-  
-  # bootstrap t-stats
-  temp_tabs = pubcross$tabs[id_all[booti, ]]
-  
-  temp_est = pifgrid_estimate(est.boot, temp_tabs, tempopts)
-  temp_bootdat = temp_est$est$par %>% 
-    mutate(
-      loglike = temp_est$est$loglike
-      , hstar   = find_hurdle(temp_est$est$par)
-    )
-  
-  if (booti == 1){
-    bootdat = temp_bootdat
-  } else {
-    bootdat = rbind(bootdat, temp_bootdat)
-  }
-  
-  print(temp_bootdat)
-  
-} # end for booti
-
-
-
-# find t-hurdles ====
-
-hlist = pubcross$tabs %>% sort()
-hlist = hlist[hlist<4]
-
-
-
-
+opt$objective
